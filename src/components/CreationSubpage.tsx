54a8d6c68a77a63b8ac070f7a6e6f590a9307e91
Nimavein <michalzygiel00@gmail.com>
2021-11-08 15:59:44 +0100

    adjust add subpage layout, add page title

--- src/components/CreationSubpage.tsx
+++ src/components/CreationSubpage.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import NewCategory from "./NewCategory";
 import NewProduct from "./NewProduct";
-import { Container } from "react-bootstrap";
+import { Col, Container } from "react-bootstrap";
 
 const CreationSubpage: React.FC = () => {
   return (
     <Container>
-      <NewCategory />
-      <NewProduct />
+      <Col lg={6}>
+        <NewCategory />
+        <NewProduct />
+      </Col>
     </Container>
   );
 };
